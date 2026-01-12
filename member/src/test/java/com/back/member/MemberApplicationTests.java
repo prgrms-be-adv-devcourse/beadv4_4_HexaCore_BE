63b197a87812ade1df7d7e30df174ec02743cdc0
Sunmin <sunmin323233@gmail.com>
2026-01-12 16:06:49 +0900

    chore: 프로젝트 및 모듈 설정

---
+++ member/src/test/java/com/back/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
