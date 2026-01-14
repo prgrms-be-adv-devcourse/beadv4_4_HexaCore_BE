bbc2c6ceecd2871419958495f034fed0937ac36a
Seunggu <83169867+Seung-9@users.noreply.github.com>
2026-01-14 15:47:20 +0900

    [FEAT] 정산 도메인 구현 (#21)
    * feat: 정산 도메인 구현 - #14
    
    * feat: 정산 상태 관리 enum 구현 - #14
    
    * feat: 정산 아이템 도메인 구현 - #14
    
    * feat: 정산 아이템 상태 관리 enum 구현 - #14
    
    * feat: 정산 수수료 정책 구현 - #14
    
    * feat: 정산 상태 변경 로그 엔티티 구현 - #14
    
    * feat: 정산 항목 로그 엔티티 구현 - #14
    
    * build: 정산 모듈 의존성 추가 - #14

---
+++ settlement/src/main/java/com/back/settlement/domain/SettlementPolicy.java
@@ -0,0 +1,8 @@
+package com.back.settlement.domain;
+
+import java.math.BigDecimal;
+
+public final class SettlementPolicy {
+
+    public static final BigDecimal FEE_RATE = new BigDecimal("0.03");
+}
