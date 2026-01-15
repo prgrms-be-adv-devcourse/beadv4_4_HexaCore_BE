4da3c0c27c5fa4c9bca5f21850572c98c14998bc
NamSangwon <nsw3265@naver.com>
2026-01-15 11:53:21 +0900

    feat: Category 엔티티 구현 - #27

---
+++ product/src/main/java/com/back/product/domain/Category.java
@@ -0,0 +1,27 @@
+package com.back.product.domain;
+
+import com.back.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@SQLDelete(sql = "UPDATE CATEGORY SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at IS NULL")
+@Table(name = "CATEGORY")
+public class Category extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String imageUrl;
+}
