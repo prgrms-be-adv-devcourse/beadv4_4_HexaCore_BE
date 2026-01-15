258e9eb94013594815a7914b39a0348090241446
NamSangwon <nsw3265@naver.com>
2026-01-15 11:42:24 +0900

    feat: Brand 엔티티 구현 - #27

---
+++ product/src/main/java/com/back/product/domain/Brand.java
@@ -0,0 +1,23 @@
+package com.back.product.domain;
+
+import com.back.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "BRAND")
+public class Brand extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String imageUrl;
+}
