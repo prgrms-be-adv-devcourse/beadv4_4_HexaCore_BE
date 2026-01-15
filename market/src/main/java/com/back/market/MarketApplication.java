d049e31a716d26de84e52ac7757220c38be650c1
Sunmin <sunmin323233@gmail.com>
2026-01-15 22:31:34 +0900

    feat: MarketApplication에 common 모듈 스캔을 위한 설정 추가 - #33
    - customException이 불러와지지 않아서 base package를 com.back으로 설정
    - common 모듈 인식 못해서 500에러 나던 것을 400으로 낮춤

--- market/src/main/java/com/back/market/MarketApplication.java
+++ market/src/main/java/com/back/market/MarketApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing //BaseTimeEntity 작동 위해 추가
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.back") // common 모듈 스캔을 위해 추가
 public class MarketApplication {
 
     public static void main(String[] args) {
