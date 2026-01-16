a4fda536bd12b989414ccefc5875ae0accf5bcd8
NamSangwon <nsw3265@naver.com>
2026-01-16 13:07:16 +0900

    chore: security 모듈 불러오기 - #28

--- product/src/main/java/com/back/product/ProductApplication.java
+++ product/src/main/java/com/back/product/ProductApplication.java
@@ -1,11 +1,20 @@
 package com.back.product;
 
+import com.back.security.config.BaseSecurityConfig;
+import com.back.security.jwt.JWTFilter;
+import com.back.security.jwt.JWTUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Import({ // Importing security configurations and utilities for use in the product module
+    BaseSecurityConfig.class,
+    JWTFilter.class,
+    JWTUtil.class
+})
 public class ProductApplication {
 
     public static void main(String[] args) {
