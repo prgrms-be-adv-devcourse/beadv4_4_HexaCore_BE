522914695bfc60650c24443d11eba2129adac8fe
Dlrjswn <lkj6387@gmail.com>
2026-01-16 11:45:11 +0900

    refactor: 컴포넌트 스캔 방식 수정 - #6

--- chat/src/main/java/com/back/chat/ChatApplication.java
+++ chat/src/main/java/com/back/chat/ChatApplication.java
@@ -1,14 +1,12 @@
 package com.back.chat;
 
-import com.back.security.config.SecurityModuleConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
-@Import(SecurityModuleConfig.class)
+
+@SpringBootApplication(scanBasePackages = "com.back")
 public class ChatApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
     }
