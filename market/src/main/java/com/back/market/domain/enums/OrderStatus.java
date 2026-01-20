051cb1082e7622b925817a42d2092774e0e9aa52
Sunmin <sunmin323233@gmail.com>
2026-01-20 18:54:34 +0900

    feat: OrderStatus에 COMPLETED 상태 추가 - #70

--- market/src/main/java/com/back/market/domain/enums/OrderStatus.java
+++ market/src/main/java/com/back/market/domain/enums/OrderStatus.java
@@ -4,5 +4,6 @@ package com.back.market.domain.enums;
 public enum OrderStatus {
     HOLD,       // 결제 대기 (낙찰 직후)
     PAID,       // 결제 완료 (거래 성사)
-    CANCELLED   // 주문 취소
+    CANCELLED,   // 주문 취소
+    COMPLETED   // 구매 확정(정산 가능)
 }
