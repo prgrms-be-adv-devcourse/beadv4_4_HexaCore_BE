b5ec6ea0d084ca57f0f4b3bd5919309a65179392
Sunmin <sunmin323233@gmail.com>
2026-01-20 15:06:54 +0900

    rename: CashHoldResponseDto를 PayAndHoldResponseDto로 수정 및 필드 추가 - #70

---
+++ market/src/main/java/com/back/market/dto/response/PayAndHoldResponseDto.java
@@ -0,0 +1,19 @@
+package com.back.market.dto.response;
+
+import com.back.market.dto.enums.PayAndHoldStatus;
+import com.back.market.dto.enums.RelType;
+
+import java.math.BigDecimal;
+
+public record PayAndHoldResponseDto(
+        PayAndHoldStatus status,
+        RelType relType,
+        Long relId,
+        BigDecimal walletUsedAmount,
+        BigDecimal pgRequiredAmount,
+        String tossOrderId
+) {
+    public static PayAndHoldResponseDto of(PayAndHoldStatus status, RelType relType, Long relId, BigDecimal walletUsedAmount, BigDecimal pgRequiredAmount, String tossOrderId) {
+        return new PayAndHoldResponseDto(status, relType, relId, walletUsedAmount, pgRequiredAmount, tossOrderId);
+    }
+}
