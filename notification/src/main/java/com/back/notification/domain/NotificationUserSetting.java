9cddfb568c94d99ea0a3e14798a238b6463f9914
jj <syeonie0526@gmail.com>
2026-01-21 15:20:12 +0900

    feat: Notification 모듈에 NotificationUserSetting 복제본 추가 - #86

---
+++ notification/src/main/java/com/back/notification/domain/NotificationUserSetting.java
@@ -0,0 +1,34 @@
+package com.back.notification.domain;
+
+import com.back.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table(name = "notification_user_settings")
+public class NotificationUserSetting extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private NotificationUser user;
+
+    @Builder.Default
+    private boolean bidStatusEnabled = true;
+
+    @Builder.Default
+    private boolean productStatusEnabled = true;
+
+    @Builder.Default
+    private boolean priceEnabled = true;
+
+    @Builder.Default
+    private boolean settlementEnabled = true;
+
+}
