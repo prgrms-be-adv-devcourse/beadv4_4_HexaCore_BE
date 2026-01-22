ce85ee8a1ad52ce9926f44c10e9ed4055679311c
Sunmin <145127753+hikigirl@users.noreply.github.com>
2026-01-22 14:15:28 +0900

    [CHORE] Swagger 설정에 JWT 사용 가능하게끔 추가 (#110)

--- common/src/main/java/com/back/common/config/SwaggerConfig.java
+++ common/src/main/java/com/back/common/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.back.common.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,8 +18,19 @@ public class SwaggerConfig {
                 .version("1.0.0")
                 .description("Resello 멀티모듈 프로젝트의 API 명세서입니다.");
 
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt); // 헤더에 토큰 포함 요청
+
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer") // swagger에서 요청 보낼때 Bearer 접두어 붙여줌
+                .bearerFormat("JWT")
+        );
+
         return new OpenAPI()
-                .components(new Components())
-                .info(info);
+                .components(components)
+                .info(info)
+                .addSecurityItem(securityRequirement);
     }
 }
