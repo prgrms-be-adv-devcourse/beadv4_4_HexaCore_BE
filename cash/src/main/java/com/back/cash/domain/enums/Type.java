472fda19dec1b03be0d3f474785f5e2787d53682
YooSejeong <nived3@naver.com>
2026-01-22 11:33:06 +0900

    refactor: CashLog Type에서 취소 상태 RELEASE를 RELEASE_ON_FAIL, REFUND_ONCANCEL로 분리 - #92

--- cash/src/main/java/com/back/cash/domain/enums/Type.java
+++ cash/src/main/java/com/back/cash/domain/enums/Type.java
@@ -3,7 +3,8 @@ package com.back.cash.domain.enums;
 public enum Type {
     TOPUP_PG,    // PG 충전
     HOLD,    // 금액 홀딩
-    RELEASE,    // 주문 취소
+    RELEASE_ON_FAIL,    // 결제 실패로 인한 선홀딩 반환
+    REFUND_ON_CANCEL, // 주문 취소로 전액 환불
     SETTLEMENT_PRINCIPAL,    // 정산
     SETTLEMENT_FEE     // 정산 수수료
 }
