0b09b6a24de5b27a936c9ab56172023f08872ee9
Sunmin <sunmin323233@gmail.com>
2026-01-22 10:29:56 +0900

    refactor: confirmPayment 메서드에 @RequestBody 추가 - #67

--- market/src/main/java/com/back/market/adapter/in/ApiV1MarketInternalController.java
+++ market/src/main/java/com/back/market/adapter/in/ApiV1MarketInternalController.java
@@ -5,6 +5,7 @@ import com.back.common.response.CommonResponse;
 import com.back.market.app.MarketInternalFacade;
 import com.back.market.dto.request.PaymentCompletedRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +15,7 @@ public class ApiV1MarketInternalController implements ApiV1MarketInternal {
     private final MarketInternalFacade marketInternalFacade;
 
     @Override
-    public CommonResponse<Void> confirmPayment(PaymentCompletedRequestDto requestDto) {
+    public CommonResponse<Void> confirmPayment(@RequestBody PaymentCompletedRequestDto requestDto) {
         boolean isSuccess = marketInternalFacade.confirmPayment(requestDto);
         if(!isSuccess) {
             return CommonResponse.success(SuccessCode.ALREADY_PROCESSED, null);
