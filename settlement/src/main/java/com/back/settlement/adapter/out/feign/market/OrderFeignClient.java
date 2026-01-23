1dbdfae7186a74d6bf9cabd7b8bc35e9e1efd4a2
Seung-9 <ssg9505fj22@naver.com>
2026-01-23 17:07:17 +0900

    chore: 불필요한 어노테이션 삭제 엔드포인트 변경 - #119

--- settlement/src/main/java/com/back/settlement/adapter/out/feign/market/OrderFeignClient.java
+++ settlement/src/main/java/com/back/settlement/adapter/out/feign/market/OrderFeignClient.java
@@ -1,7 +1,6 @@
 package com.back.settlement.adapter.out.feign.market;
 
 import com.back.common.dto.settlement.SettlementTargetOrder;
-import org.springframework.context.annotation.Profile;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
@@ -11,11 +10,10 @@ import java.time.YearMonth;
 import java.util.List;
 
 @HttpExchange
-@Profile("!test & !local")
 public interface OrderFeignClient extends OrderClient {
 
     @Override
-    @GetExchange("/api/v1/orders/settlement-target")
+    @GetExchange("/api/v1/internal/market/orders/settlement-target")
     List<SettlementTargetOrder> findSettlementTargetOrders(
             @RequestParam("targetMonth") @DateTimeFormat(pattern = "yyyy-MM") YearMonth targetMonth,
             @RequestParam("page") int page,
