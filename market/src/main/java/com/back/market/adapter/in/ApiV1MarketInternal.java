de35f2bd32545c3045b0ca294d3063766bfb7ef5
Sunmin <sunmin323233@gmail.com>
2026-01-23 12:50:57 +0900

    chore: 결제 실패 관련 TODO 주석 추가 - #118

--- market/src/main/java/com/back/market/adapter/in/ApiV1MarketInternal.java
+++ market/src/main/java/com/back/market/adapter/in/ApiV1MarketInternal.java
@@ -15,4 +15,7 @@ public interface ApiV1MarketInternal {
     @Operation(summary = "결제 완료 통지 수신 (Internal Callback)", description = "Cash 모듈로부터 결제 완료(입금 확인) 통지를 수신하여 주문 상태를 확정한다.")
     @PostMapping("/payments/confirm")
     CommonResponse<Void> confirmPayment(@RequestBody PaymentCompletedRequestDto requestDto);
+
+    // TODO 사용자가 결제 도중 실패했을 때 받아주는 컨트롤러 없음
+    // /api/v1/internal/market/payments/fail
 }
