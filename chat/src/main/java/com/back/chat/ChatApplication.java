e09e57882cb221cca0c5a228f8d90d1bfe25f145
이건주 <102896954+Dlrjswn@users.noreply.github.com>
2026-01-30 16:28:18 +0900

    [FIX] 웹소켓 브로드캐스트 오류 수정 (#195)
    * [FIX] Redis 채널 패턴 불일치 수정 - #191
    
    * [FIX] EnableJpaAuditing 어노테이션 적용 - #191
    
    * [FEAT] RedisChatMessageSubscriberTest 구현 - #191

--- chat/src/main/java/com/back/chat/ChatApplication.java
+++ chat/src/main/java/com/back/chat/ChatApplication.java
@@ -4,10 +4,12 @@ package com.back.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication(scanBasePackages = "com.back")
 @EnableFeignClients
+@EnableJpaAuditing
 public class ChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
