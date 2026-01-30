9a7d96d3db08d52285874214db3762ed97356993
YooSejeong <nived3@naver.com>
2026-01-30 09:57:17 +0900

    fix: 테스트 로그인에서 refresh 토큰 쿠키에 담기도록 수정 - #159

--- user/src/main/java/com/back/user/adapter/in/auth/AuthController.java
+++ user/src/main/java/com/back/user/adapter/in/auth/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
-public class ReissueController {
+public class AuthController {
 
     private final ReissueTokenUseCase reissueTokenUseCase;
 
--- user/src/main/java/com/back/user/adapter/in/auth/TestLoginController.java
+++ user/src/main/java/com/back/user/adapter/in/auth/TestLoginController.java
@@ -5,6 +5,7 @@ import com.back.common.response.CommonResponse;
 import com.back.user.app.auth.TestLoginUseCase;
 import com.back.user.dto.request.TestLoginRequestDto;
 import com.back.user.dto.response.TestLoginResponseDto;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
@@ -22,10 +23,8 @@ public class TestLoginController {
     private final TestLoginUseCase testLoginUseCase;
 
     @PostMapping("/test-login")
-    public CommonResponse<TestLoginResponseDto> testLogin(@RequestBody TestLoginRequestDto req) {
+    public CommonResponse<TestLoginResponseDto> testLogin(@RequestBody TestLoginRequestDto req, HttpServletResponse response) {
         TestLoginUseCase.Result r = testLoginUseCase.execute(req.email());
-
-        HttpHeaders headers = new HttpHeaders();
 
         ResponseCookie refreshCookie = ResponseCookie.from("refresh", r.refreshToken())
                 .httpOnly(true)
@@ -35,7 +34,7 @@ public class TestLoginController {
                 .maxAge(r.refreshTtl())
                 .build();
 
-        headers.add(HttpHeaders.SET_COOKIE, refreshCookie.toString());
+        response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
         return CommonResponse.success(SuccessCode.OK, new TestLoginResponseDto(r.accessToken()));
     }
 
