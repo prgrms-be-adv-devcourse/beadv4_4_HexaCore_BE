7bb38194b45c0df7374fa77ad676388efb29777f
NamSangwon <nsw3265@naver.com>
2026-01-30 15:34:08 +0900

    refactor: swagger 설명 오류 수정 - #164

--- product/src/main/java/com/back/product/adapter/in/OptionApiController.java
+++ product/src/main/java/com/back/product/adapter/in/OptionApiController.java
@@ -48,7 +48,7 @@ public interface OptionApiController {
         기존의 옵션 그룹을 삭제합니다.
         삭제 시, 그룹 내의 값들을 모두 확인해서 사용되는 상품이 있다면, 삭제를 진행하지 않습니다.
     """)
-    @ApiResponse(responseCode = "204", description = "옵션 값 수정 성공")
+    @ApiResponse(responseCode = "204", description = "옵션 그룹 삭제 성공")
     @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content)
     @ApiResponse(responseCode = "403", description = "권한 없음", content = @Content)
     @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
