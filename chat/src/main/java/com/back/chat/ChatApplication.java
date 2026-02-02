277c312bc4709f1d658dff6d9a533a6801699a88
Dlrjswn <lkj6387@gmail.com>
2026-02-02 17:13:54 +0900

    hotfix: chat모듈 패키지 파일 설정 변경 - #217

--- chat/src/main/java/com/back/chat/ChatApplication.java
+++ chat/src/main/java/com/back/chat/ChatApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication(scanBasePackages = {
+        "com.back.chat",
         "com.back.common",
         "com.back.security"
 })
