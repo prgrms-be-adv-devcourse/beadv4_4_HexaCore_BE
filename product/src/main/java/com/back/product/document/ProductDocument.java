9ffd4c06ef4e6898034af91fd3899be5651e3422
NamSangwon <nsw3265@naver.com>
2026-02-04 08:47:39 +0900

    fix: elasticsearch 설정 추가 - #171

--- product/src/main/java/com/back/product/document/ProductDocument.java
+++ product/src/main/java/com/back/product/document/ProductDocument.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Document(indexName = "products")
+@Setting(settingPath = "elasticsearch-settings.json")
 public class ProductDocument extends BaseDocument<String> {
 
     @Field(type = FieldType.Long)
