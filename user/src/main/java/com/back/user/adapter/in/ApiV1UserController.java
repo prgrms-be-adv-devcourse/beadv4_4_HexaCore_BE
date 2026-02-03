f3aa30cf0990375e1521e010ffd658dbedf0f2f3
YooSejeong <nived3@naver.com>
2026-02-03 17:30:40 +0900

    feat: 회원 컨트롤러에 회원 정보 수정 API 추가 - #160

--- user/src/main/java/com/back/user/adapter/in/ApiV1UserController.java
+++ user/src/main/java/com/back/user/adapter/in/ApiV1UserController.java
@@ -6,7 +6,9 @@ import com.back.security.principal.AuthPrincipal;
 import com.back.user.app.UserFacade;
 import com.back.user.dto.request.UpdateFcmTokenRequest;
 import com.back.user.dto.request.UpdateNotificationSettingsRequest;
+import com.back.user.dto.request.UpdateUserProfileRequestDto;
 import com.back.user.dto.response.NotificationSettingResponse;
+import com.back.user.dto.response.UpdateUserProfileResponseDto;
 import com.back.user.dto.response.UserIdResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -42,4 +44,9 @@ public class ApiV1UserController implements UserApiV1 {
         return CommonResponse.success(SuccessCode.OK, response);
     }
 
+    @PatchMapping("/me/profile")
+    public CommonResponse<UpdateUserProfileResponseDto> updateUserProfile(@AuthenticationPrincipal AuthPrincipal authPrincipal, @Valid @RequestBody UpdateUserProfileRequestDto request) {
+        UpdateUserProfileResponseDto response = userFacade.updateUserProfile(authPrincipal.getUserId(), request);
+        return CommonResponse.success(SuccessCode.OK, response);
+    }
 }
