bc36e16e06cd84cb76153e601cee2b6310b81979
Dlrjswn <lkj6387@gmail.com>
2026-02-04 09:46:39 +0900

    refactor: ChatRoom 클래스 생성자 구현 - #232

--- chat/src/main/java/com/back/chat/domain/ChatRoom.java
+++ chat/src/main/java/com/back/chat/domain/ChatRoom.java
@@ -2,11 +2,13 @@ package com.back.chat.domain;
 
 import com.back.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Entity
-@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class ChatRoom extends BaseTimeEntity {
 
@@ -16,4 +18,8 @@ public class ChatRoom extends BaseTimeEntity {
 
     @Column(name = "brand_id", nullable = false, unique = true)
     private Long brandId;
+
+    public ChatRoom(Long brandId) {
+        this.brandId = brandId;
+    }
 }
