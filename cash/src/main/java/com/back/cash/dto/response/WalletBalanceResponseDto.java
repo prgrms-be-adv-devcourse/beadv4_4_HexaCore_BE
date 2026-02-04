6765be2fcf04801b58c963bb3fd02088dbfef9f0
YooSejeong <nived3@naver.com>
2026-02-04 16:04:13 +0900

    feat: 지갑 잔액 응답 dto 추가 - #244

---
+++ cash/src/main/java/com/back/cash/dto/response/WalletBalanceResponseDto.java
@@ -0,0 +1,7 @@
+package com.back.cash.dto.response;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record WalletBalanceResponseDto(Long walletId, BigDecimal balance, LocalDateTime lastModifiedAt) {
+}
