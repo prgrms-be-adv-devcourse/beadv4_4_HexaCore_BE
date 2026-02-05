3bf3aa04f76a1de8e84cd7713513f9ce43f56ec5
YooSejeong <nived3@naver.com>
2026-02-05 09:56:39 +0900

    style: 공백 제거 - #244

--- cash/src/main/java/com/back/cash/adapter/in/CashApiV1.java
+++ cash/src/main/java/com/back/cash/adapter/in/CashApiV1.java
@@ -14,7 +14,7 @@ public interface CashApiV1 {
     @Operation(
             summary = "내 지갑 잔액 조회",
             description = """
-                로그인한 사용자의 지갑 잔액을 조회합니다.            
+                로그인한 사용자의 지갑 잔액을 조회합니다.
                 """
     )
     @ApiResponse(responseCode = "200", description = "지갑 잔액 조회 성공")
