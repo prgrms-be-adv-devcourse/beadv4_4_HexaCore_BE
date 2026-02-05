7bd1237b7ca2087cc6b5621b93acee83234f26d8
YooSejeong <nived3@naver.com>
2026-02-05 09:43:28 +0900

    refactor: 응답 dto 개행 추가 -#244

--- cash/src/main/java/com/back/cash/dto/response/WalletBalanceResponseDto.java
+++ cash/src/main/java/com/back/cash/dto/response/WalletBalanceResponseDto.java
@@ -3,5 +3,9 @@ package com.back.cash.dto.response;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record WalletBalanceResponseDto(Long walletId, BigDecimal balance, LocalDateTime lastModifiedAt) {
+public record WalletBalanceResponseDto(
+        Long walletId,
+        BigDecimal balance,
+        LocalDateTime lastModifiedAt
+) {
 }
