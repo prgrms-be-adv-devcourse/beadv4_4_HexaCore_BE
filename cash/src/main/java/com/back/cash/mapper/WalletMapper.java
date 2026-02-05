e6def1fec5f605180d09c4eeef501ad61f43fe0a
YooSejeong <nived3@naver.com>
2026-02-05 09:44:36 +0900

    feat: Builder 패턴 적용 - #244

--- cash/src/main/java/com/back/cash/mapper/WalletMapper.java
+++ cash/src/main/java/com/back/cash/mapper/WalletMapper.java
@@ -6,10 +6,10 @@ import com.back.cash.dto.response.WalletBalanceResponseDto;
 public class WalletMapper {
 
     public static WalletBalanceResponseDto toWalletBalanceResponseDto(Wallet wallet) {
-        return new WalletBalanceResponseDto(
-                wallet.getId(),
-                wallet.getBalance(),
-                wallet.getLastModifiedAt()
-        );
+        return WalletBalanceResponseDto.builder()
+                .walletId(wallet.getId())
+                .balance(wallet.getBalance())
+                .lastModifiedAt(wallet.getLastModifiedAt())
+                .build();
     }
 }
