77befb46a93d5880701f36339a82e069d54ca896
Seung-9 <ssg9505fj22@naver.com>
2026-02-08 21:58:56 +0900

    feat: 상태 전이 규칙 추가 - #268

--- settlement/src/main/java/com/back/settlement/domain/SettlementItemStatus.java
+++ settlement/src/main/java/com/back/settlement/domain/SettlementItemStatus.java
@@ -1,11 +1,23 @@
 package com.back.settlement.domain;
 
-import lombok.Getter;
+import java.util.Set;
 
-@Getter
 public enum SettlementItemStatus {
+    COLLECTED, // 수집됨: 일일 배치에서 수집된 미처리 항목
     INCLUDED,  // 포함됨: 정산에 포함된 정상 거래
     CANCELED,  // 취소됨: 주문 취소로 정산 제외
     REFUNDED,  // 환불됨: 환불 처리되어 정산 제외
-    NEGATIVE   // 마이너스: 환불로 인한 차감 항목
+    NEGATIVE;  // 마이너스: 환불로 인한 차감 항목
+
+    public Set<SettlementItemStatus> getAllowedTransitions() {
+        return switch (this) {
+            case COLLECTED -> Set.of(INCLUDED);
+            case INCLUDED -> Set.of(CANCELED, REFUNDED, NEGATIVE);
+            case CANCELED, REFUNDED, NEGATIVE -> Set.of();
+        };
+    }
+
+    public boolean canTransitionTo(SettlementItemStatus targetStatus) {
+        return getAllowedTransitions().contains(targetStatus);
+    }
 }
