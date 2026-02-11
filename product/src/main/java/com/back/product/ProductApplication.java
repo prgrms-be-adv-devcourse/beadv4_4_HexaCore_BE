b87b9f69e9442f00744639908f9442b20d452a4f
NamSangwon <nsw3265@naver.com>
2026-02-11 15:20:19 +0900

    chore: product 모듈에 ai 모듈 추가 - #281

--- product/src/main/java/com/back/product/ProductApplication.java
+++ product/src/main/java/com/back/product/ProductApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
         "com.back.common",
         "com.back.security",
         "com.back.image",
+        "com.back.ai",
         "com.back.security"
 })
 public class ProductApplication {
