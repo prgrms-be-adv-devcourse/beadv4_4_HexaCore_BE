f645268ee9ee17b9caa10193ea1b107c6c2c4d81
YooSejeong <nived3@naver.com>
2026-02-11 10:55:45 +0900

    refactor: 소셜 로그인시 로그 수정 - #279

--- user/src/main/java/com/back/user/adapter/in/auth/security/oauth/CustomOAuth2UserService.java
+++ user/src/main/java/com/back/user/adapter/in/auth/security/oauth/CustomOAuth2UserService.java
@@ -40,7 +40,6 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        log.info("소셜 로그인한 사용자 정보 : {}", oAuth2User.getAttributes());
 
         String provider = userRequest.getClientRegistration().getRegistrationId();
 
@@ -50,6 +49,9 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             case "kakao" -> new KakaoResponse(oAuth2User.getAttributes());
             default -> throw new OAuth2AuthenticationException("지원하지 않는 소셜 로그인입니다: " + provider);
         };
+
+        log.info("[SOCIAL_LOGIN_ATTEMPT] provider: {}, email: {}, providerId: {}",
+                provider, response.getEmail(), response.getProviderId());
 
         Provider authProvider = response.getProvider();
         String providerId = response.getProviderId();
