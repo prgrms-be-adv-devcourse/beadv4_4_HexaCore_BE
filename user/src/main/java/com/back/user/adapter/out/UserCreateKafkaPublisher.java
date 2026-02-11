797e14b02bf820e28376ed9eb75493c63b648054
YooSejeong <nived3@naver.com>
2026-02-11 10:47:47 +0900

    refactor: 회원 가입시 replica, 지갑 생성 카프카 이벤트 발행에서 토픽 이름 수정 및 envelop 적용 - #279

--- user/src/main/java/com/back/user/adapter/out/UserCreateKafkaPublisher.java
+++ user/src/main/java/com/back/user/adapter/out/UserCreateKafkaPublisher.java
@@ -1,10 +1,12 @@
 package com.back.user.adapter.out;
 
-import com.back.common.user.event.UserCreatedEvent;
+import com.back.common.event.Envelope;
+import com.back.common.event.KafkaEventPublisher;
+import com.back.user.app.event.UserCreatedPayload;
+import com.back.user.domain.event.UserCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -14,17 +16,26 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Slf4j
 public class UserCreateKafkaPublisher {
 
-    @Value("${custom.kafka.topic.user-created}")
+    @Value("${custom.kafka.topic.user-account-created}")
     private String userCreatedTopic;
 
-    private final KafkaTemplate<String, UserCreatedEvent> kafkaTemplate;
+    private final KafkaEventPublisher kafkaEventPublisher;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void publish(UserCreatedEvent event) {
-        kafkaTemplate.send(userCreatedTopic, String.valueOf(event.id()), event)
-                .whenComplete((res, ex) -> {
-                    if (ex != null) log.error("UserCreated 이벤트 전송 실패 userId={}", event.id(), ex);
-                    else log.info("UserCreated 이벤트 전송 성공 userId={}", event.id());
-                });
+        Envelope<UserCreatedPayload> envelope = Envelope.of("user.account.created", new UserCreatedPayload(
+                event.id(),
+                event.nickname(),
+                event.name(),
+                event.email(),
+                event.address(),
+                event.phone(),
+                event.profileImageUrl()
+        ));
+
+        kafkaEventPublisher.publish(userCreatedTopic, envelope);
+
+        log.info("[USER_CREATED_KAFKA_PUBLISH eventId={}, occurredAt={}, topic={}, userId={}",
+                envelope.header().eventId(), envelope.header().occurrenceAt(), userCreatedTopic, event.id());
     }
 }
