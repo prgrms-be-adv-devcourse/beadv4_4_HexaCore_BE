711d387ce1ea6a6f94de3429d5200c2d5b014d65
jj <syeonie0526@gmail.com>
2026-02-11 11:11:07 +0900

    refactor: 알림 kafka 토픽명 yml화  - (#290)

--- notification/src/main/java/com/back/notification/adapter/in/NotificationEventListener.java
+++ notification/src/main/java/com/back/notification/adapter/in/NotificationEventListener.java
@@ -20,39 +20,60 @@ public class NotificationEventListener {
     private final NotificationFacade notificationFacade;
     private final NotificationUserUsecase notificationUserUsecase;
 
-    @KafkaListener(topics = "BiddingCompletedEvent", groupId = "PostEventListener__handle__1")
+    @KafkaListener(
+            topics = "${custom.kafka.topic.market-order-completed}",
+            groupId = "${custom.kafka.consumer.group-id}"
+    )
     public void handle(BiddingCompletedEvent event) {
         notificationFacade.notify(Type.BID_COMPLETED, event);
     }
 
-    @KafkaListener(topics = "PurchaseCanceledEvent", groupId = "PostEventListener__handle__2")
+    @KafkaListener(
+            topics = "${custom.kafka.topic.market-order-deleted}",
+            groupId = "${custom.kafka.consumer.group-id}"
+    )
     public void handle(PurchaseCanceledEvent event) {
         notificationFacade.notify(Type.PURCHASE_CANCELED, event);
     }
 
     // 30일 초과되어 입찰에 실패
-    @KafkaListener(topics = "BiddingFailedEvent", groupId = "PostEventListener__handle__3")
+    @KafkaListener(
+            topics = "${custom.kafka.topic.market-bidding-deleted}",
+            groupId = "${custom.kafka.consumer.group-id}"
+    )
     public void handle(BiddingFailedEvent event) {
         notificationFacade.notify(Type.BID_FAILED, event);
     }
 
-    @KafkaListener(topics = "InspectionCompletedEvent", groupId = "PostEventListener__handle__1")
+    @KafkaListener(
+            topics = "${custom.kafka.topic.product-inspect-completed}",
+            groupId = "${custom.kafka.consumer.group-id}"
+    )
     public void handle(InspectionCompletedEvent event) {
         notificationFacade.notify(Type.INSPECTION_COMPLETED, event);
     }
 
     // 정산 완료
-    @KafkaListener(topics = "SettlementCompletedEvent", groupId = "PostEventListener__handle__4")
+    @KafkaListener(
+            topics = "${custom.kafka.topic.settlement-payout-completed}",
+            groupId = "${custom.kafka.consumer.group-id}"
+    )
     public void handle(SettlementCompletedEvent event) {
         notificationFacade.notify(Type.SETTLEMENT_COMPLETED, event);
     }
 
-    @KafkaListener(topics = "SellBiddingCreatedEvent", groupId = "PostEventListener__handle__5")
+    @KafkaListener(
+            topics = "${custom.kafka.topic.market-bidding-sell-created}",
+            groupId = "${custom.kafka.consumer.group-id}"
+    )
     public void handle(SellBiddingCreatedEvent event) {
         notificationFacade.notify(Type.PRICE_DROPPED, event);
     }
 
-    @KafkaListener(topics = "fcmTokenChangedEvent", groupId = "PostEventListener__handle__6")
+    @KafkaListener(
+            topics = "${custom.kafka.topic.user-account-updated}",
+            groupId = "${custom.kafka.consumer.group-id}"
+    )
     public void handle(FcmTokenChangedEvent event) {
         notificationUserUsecase.updateFcmToken(event.userId(), event.fcmToken());
     }
