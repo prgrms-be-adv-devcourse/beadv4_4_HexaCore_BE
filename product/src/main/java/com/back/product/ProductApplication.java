76678329ff8534d1fab28f1769b384791c6fc3b4
jj <syeonie0526@gmail.com>
2026-02-11 10:24:25 +0900

    refactor: Market과 Product 모듈에서 detector 의존성 삭제 및이상 감지 어노테이션 구현 삭제  - (#230)

--- product/src/main/java/com/back/product/ProductApplication.java
+++ product/src/main/java/com/back/product/ProductApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
         "com.back.common",
         "com.back.security",
         "com.back.image",
-        "com.back.security",
-        "com.back.detector"
+        "com.back.security"
 })
 public class ProductApplication {
 
