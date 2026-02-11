bad82a4b6a5b87bb329a46f0f0fe4e4133407238
YooSejeong <nived3@naver.com>
2026-02-11 10:45:21 +0900

    feat: 회원 모듈에 회원 가입시 replica, 지갑 생성 요청 이벤트 추가 - #279

---
+++ user/src/main/java/com/back/user/domain/event/UserCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.back.user.domain.event;
+
+import com.back.common.event.EventName;
+
+public record UserCreatedEvent(
+        Long id,
+        String nickname,
+        String name,
+        String email,
+        String address,
+        String phone,
+        String profileImageUrl
+) implements EventName {
+}
