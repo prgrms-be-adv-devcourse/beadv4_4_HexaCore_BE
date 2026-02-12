f165010ec5b6284ea7fa739291d446bb7d607cd8
Seung-9 <ssg9505fj22@naver.com>
2026-02-12 10:43:19 +0900

    refactor: 정산 모듈 책임 범위 재정의에 따른 불필요한 파일 제거 및 수정 - 298

--- settlement/src/main/java/com/back/settlement/app/event/payload/PayoutResultPayload.java
+++
@@ -1,9 +0,0 @@
-package com.back.settlement.app.event.payload;
-
-import com.back.common.event.KafkaPayload;
-
-public record PayoutResultPayload(
-        Long settlementId,
-        boolean success,
-        String failReason
-) implements KafkaPayload {}
