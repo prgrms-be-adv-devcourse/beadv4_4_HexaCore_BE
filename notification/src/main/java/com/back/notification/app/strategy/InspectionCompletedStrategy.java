7c33e8fe29c920be517af5da0f89441f27f481c0
jj <syeonie0526@gmail.com>
2026-02-13 15:51:30 +0900

    refactor: Strategy 구현체에 @Override 사용되지 않은 메소드에 @Override 추가- (#290)

--- notification/src/main/java/com/back/notification/app/strategy/InspectionCompletedStrategy.java
+++ notification/src/main/java/com/back/notification/app/strategy/InspectionCompletedStrategy.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class InspectionCompletedStrategy implements NotificationStrategy<InspectionCompletedPayload> {
     private final NotificationMapper mapper;
 
+    @Override
     public Type type(){
         return Type.INSPECTION_COMPLETED;
     }
--- notification/src/main/java/com/back/notification/app/strategy/SettlementCompletedStrategy.java
+++ notification/src/main/java/com/back/notification/app/strategy/SettlementCompletedStrategy.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class SettlementCompletedStrategy implements NotificationStrategy<SettlementCompletedPayload> {
     private final NotificationMapper mapper;
 
+    @Override
     public Type type(){
         return Type.SETTLEMENT_COMPLETED;
     }
