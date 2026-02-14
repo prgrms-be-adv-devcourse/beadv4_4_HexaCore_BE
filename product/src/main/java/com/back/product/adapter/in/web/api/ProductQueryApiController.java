49dbe198569e83d06e687f4301fdbe54f5e0fc53
NamSangwon <nsw3265@naver.com>
2026-02-14 16:36:26 +0900

    refactor: 유사 상품 검색 API 설명 수정 - #281

--- product/src/main/java/com/back/product/adapter/in/web/api/ProductQueryApiController.java
+++ product/src/main/java/com/back/product/adapter/in/web/api/ProductQueryApiController.java
@@ -34,7 +34,7 @@ public interface ProductQueryApiController {
     @Operation(summary = "유사 상품 조회", description = """
             특정 상품과 유사한 상품들을 조회합니다.
             KNN 알고리즘을 활용하여 유사한 상품들을 추천합니다.
-            추천할 상품 수는 'count' 파라미터로 지정할 수 있습니다.
+            추천할 상품 수는 'size' 파라미터로 지정할 수 있습니다.
     """)
     @ApiResponse(responseCode = "200", description = "상품 목록 조회 성공")
     @ApiResponse(responseCode = "500", description = "서버 오류", content = @Content)
