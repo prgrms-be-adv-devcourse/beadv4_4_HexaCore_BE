5e848eda928bb907ca64740a25258d1655a83b99
NamSangwon <nsw3265@naver.com>
2026-02-17 03:13:46 +0900

    fix: 잘못된 어노테이션 적용 수정 - #308

--- product/src/main/java/com/back/product/event/kafka/BrandCreatedPayload.java
+++ product/src/main/java/com/back/product/event/kafka/BrandCreatedPayload.java
@@ -2,14 +2,14 @@ package com.back.product.event.kafka;
 
 import com.back.common.event.KafkaPayload;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
 public record BrandCreatedPayload(
-        @NotEmpty(message = "브랜드 페이로드는 필수입니다.")
+        @NotNull(message = "브랜드 페이로드는 필수입니다.")
         @Valid
         List<BrandPayload> brandPayloadList
 ) implements KafkaPayload {
