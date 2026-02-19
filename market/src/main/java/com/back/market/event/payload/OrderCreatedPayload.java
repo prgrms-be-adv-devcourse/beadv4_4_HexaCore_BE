9ac38c65c399377648d1b6268417a4c9b0df1146
Sunmin <sunmin323233@gmail.com>
2026-02-19 17:11:05 +0900

    refactor: OrderCreatedPayload의 biddingId 필드를 orderId로 변경 - #180

--- market/src/main/java/com/back/market/event/payload/OrderCreatedPayload.java
+++ market/src/main/java/com/back/market/event/payload/OrderCreatedPayload.java
@@ -5,7 +5,7 @@ import com.back.common.event.KafkaPayload;
 import java.math.BigDecimal;
 
 public record OrderCreatedPayload (
-        Long biddingId,
+        Long orderId,
         Long buyerUserId,
         Long sellerUserId,
         Long productId,
@@ -16,7 +16,7 @@ public record OrderCreatedPayload (
         BigDecimal price,
         String biddingPosition  // 판매입찰 / 구매입찰
 ) implements KafkaPayload {
-    public static OrderCreatedPayload of(Long biddingId, Long buyerUserId, Long sellerUserId, Long productId, String productName, String productSize, String thumbnailImage, String brandName, BigDecimal price, String biddingPosition) {
-        return new OrderCreatedPayload(biddingId, buyerUserId, sellerUserId, productId, productName, productSize, thumbnailImage, brandName, price, biddingPosition);
+    public static OrderCreatedPayload of(Long orderId, Long buyerUserId, Long sellerUserId, Long productId, String productName, String productSize, String thumbnailImage, String brandName, BigDecimal price, String biddingPosition) {
+        return new OrderCreatedPayload(orderId, buyerUserId, sellerUserId, productId, productName, productSize, thumbnailImage, brandName, price, biddingPosition);
     }
 }
