d7b3e11677ca9904eec034af4172a2a911492871
YooSejeong <nived3@naver.com>
2026-02-19 11:09:17 +0900

    refactor: 정산 역직렬화 예외 클래스 삭제 - #250

--- cash/src/main/java/com/back/cash/adapter/in/listener/exception/PayoutMessageParseException.java
+++
@@ -1,7 +0,0 @@
-package com.back.cash.adapter.in.listener.exception;
-
-public class PayoutMessageParseException extends RuntimeException {
-    public PayoutMessageParseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
