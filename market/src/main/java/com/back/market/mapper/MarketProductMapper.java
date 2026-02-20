033d6fc9b954f09621bbb387793725ea6caa1265
Sunmin <sunmin323233@gmail.com>
2026-02-20 14:47:30 +0900

    feat: MarketProduct 매핑 로직 및 도메인 수정 - #179
    - MarketProduct Entity에 productInfoId 및 productOptionId 필드 추가
    - MarketProductMapper의 toEntity 메서드 매개변수 및 매핑 로직 수정
    - Kafka 이벤트와 연동 위한 fromProductCreatedPayload 메서드 추가
    - 테스트 케이스에 필요한 MarketProduct 필드 정보 반영

--- market/src/main/java/com/back/market/mapper/MarketProductMapper.java
+++ market/src/main/java/com/back/market/mapper/MarketProductMapper.java
@@ -1,15 +1,28 @@
 package com.back.market.mapper;
 
 import com.back.market.domain.MarketProduct;
+import com.back.market.event.payload.OptionPayload;
+import com.back.market.event.payload.ProductCreatedPayload;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Component
 public class MarketProductMapper {
-    public MarketProduct toEntity(Long id, String brandName, String name, String productNumber,String productOption, Long price, String categoryName, String thumbnailImage) {
+    public MarketProduct toEntity(
+            Long productOptionId,
+            Long productInfoId,
+            String brandName,
+            String name,
+            String productNumber,
+            String productOption,
+            Long price,
+            String categoryName,
+            String thumbnailImage
+    ) {
         return MarketProduct.builder()
-                .id(id)
+                .productOptionId(productOptionId)
+                .productInfoId(productInfoId)
                 .brandName(brandName)
                 .name(name)
                 .productNumber(productNumber)
@@ -17,6 +30,29 @@ public class MarketProductMapper {
                 .releasePrice(BigDecimal.valueOf(price)) // 편의상 long으로 받아 여기에서 BigDecimal 변환
                 .categoryName(categoryName)
                 .thumbnailImage(thumbnailImage)
+                .build();
+    }
+
+    /**
+     * Kafka 이벤트를 통해 들어온 Payload를 엔티티로 변환하는 메서드
+     * @param payload ProductCreatedPayload
+     * @param optionValue OptionPayload.ValuePayload
+     * @return MarketProduct 엔티티
+     */
+    public MarketProduct fromProductCreatedPayload(
+            ProductCreatedPayload payload,
+            OptionPayload.ValuePayload optionValue
+    ) {
+        return MarketProduct.builder()
+                .productOptionId(optionValue.valueId())
+                .productInfoId(payload.productInfo().productInfoId())
+                .name(payload.productInfo().name())
+                .productOption(optionValue.valueName()) // 예: "230", "240"
+                .productNumber(payload.productInfo().code())
+                .thumbnailImage(payload.thumbnailUrl())
+                .releasePrice(payload.productInfo().releasePrice())
+                .brandName(payload.productInfo().brand().name())
+                .categoryName(payload.productInfo().category().name())
                 .build();
     }
 }
