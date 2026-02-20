ad899461fd3c50a33e08d5f624b84b69b64488f1
Sunmin <sunmin323233@gmail.com>
2026-02-20 15:37:37 +0900

    feat: ProductCreatedResultPayload 필드 및 Builder 추가 - #179

--- market/src/main/java/com/back/market/event/resultpayload/ProductCreatedResultPayload.java
+++ market/src/main/java/com/back/market/event/resultpayload/ProductCreatedResultPayload.java
@@ -1,4 +1,20 @@
 package com.back.market.event.resultpayload;
 
-public record ProductCreatedResultPayload() {
+import com.back.common.event.EventName;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+
+@Builder
+public record ProductCreatedResultPayload(
+        Long productOptionId,    // 원본 옵션 ID (비즈니스 키)
+        Long productInfoId,      // 원본 상품 정보 ID
+        String name,             // 상품명
+        String productOption,    // 사이즈(옵션)
+        String productNumber,    // 제품번호
+        String thumbnailImage,   // 이미지 URL
+        BigDecimal releasePrice, // 발매가
+        String brandName,        // 브랜드명
+        String categoryName      // 카테고리명
+) implements EventName {
 }
