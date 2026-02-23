8d857e2d1fc3fb5ceaabd562ef60ad4e6fcf7078
NamSangwon <nsw3265@naver.com>
2026-02-23 09:11:28 +0900

    fix: 누락된 스케쥴링 활성화 설정 추가 - #303

--- product/src/main/java/com/back/product/global/config/AuditConfig.java
+++ product/src/main/java/com/back/product/global/config/AuditConfig.java
@@ -5,9 +5,11 @@ import org.springframework.data.elasticsearch.config.EnableElasticsearchAuditing
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableAsync
+@EnableScheduling
 @EnableJpaAuditing
 @EnableElasticsearchAuditing
 @EnableElasticsearchRepositories
