00fa2f9323d1721d23d157362420f1dcaf16338c
NamSangwon <nsw3265@naver.com>
2026-02-23 16:11:42 +0900

    fix: 기본 KafkaTemplate 설정 도입 - #304

--- product/src/main/java/com/back/product/global/config/KafkaConfig.java
+++ product/src/main/java/com/back/product/global/config/KafkaConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +17,15 @@ import java.util.Map;
 public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String servers;
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configs));
+    }
 
     @Bean
     public KafkaTemplate<String, String> retryKafkaTemplate() {
