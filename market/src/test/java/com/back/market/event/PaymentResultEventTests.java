101b4103a9e2b81123c6afae6bade1fcf212fc11
Sunmin <sunmin323233@gmail.com>
2026-02-24 10:54:15 +0900

    remove: 불필요한 import 삭제 - #329

--- market/src/test/java/com/back/market/event/PaymentResultEventTests.java
+++ market/src/test/java/com/back/market/event/PaymentResultEventTests.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.KafkaTemplate;
 
 import java.math.BigDecimal;
 
