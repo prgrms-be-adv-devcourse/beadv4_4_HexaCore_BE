7cacbd20f92c8e56551ceea3fdf5c8f69b4516f6
jj <syeonie0526@gmail.com>
2026-02-25 15:19:37 +0900

    feat: ProductFeignClient 타임아웃 설정 추가 - (#349)

--- notification/src/main/java/com/back/notification/config/HttpClientConfig.java
+++ notification/src/main/java/com/back/notification/config/HttpClientConfig.java
@@ -4,6 +4,7 @@ import com.back.notification.adapter.out.feign.product.ProductFeignClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -14,14 +15,26 @@ public class HttpClientConfig {
     @Value("${feign.product.url}")
     private String productServiceUrl;
 
+    @Value("${feign.timeout.connect:3000}")
+    private int connectTimeout;
+
+    @Value("${feign.timeout.read:5000}")
+    private int readTimeout;
+
     @Bean
     public ProductFeignClient productFeignClient() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(connectTimeout);
+        factory.setReadTimeout(readTimeout);
+
         RestClient restClient = RestClient.builder()
                 .baseUrl(productServiceUrl)
+                .requestFactory(factory)
                 .build();
-        HttpServiceProxyFactory factory = HttpServiceProxyFactory
+
+        return HttpServiceProxyFactory
                 .builderFor(RestClientAdapter.create(restClient))
-                .build();
-        return factory.createClient(ProductFeignClient.class);
+                .build()
+                .createClient(ProductFeignClient.class);
     }
 }
