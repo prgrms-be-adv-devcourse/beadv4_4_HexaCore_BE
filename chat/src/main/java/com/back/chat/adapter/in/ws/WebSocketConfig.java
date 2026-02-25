0f203e2c9ffcebdb46238f5315221db06f920e1b
이건주 <102896954+Dlrjswn@users.noreply.github.com>
2026-02-25 16:20:09 +0900

    [HOTFIX] CORS 설정 제거 (#368)

--- chat/src/main/java/com/back/chat/adapter/in/ws/WebSocketConfig.java
+++ chat/src/main/java/com/back/chat/adapter/in/ws/WebSocketConfig.java
@@ -48,7 +48,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
-                .setAllowedOriginPatterns("https://www.resello.co.kr")
                 .addInterceptors(new WsHandshakeInterceptor())
                 .withSockJS();
     }
