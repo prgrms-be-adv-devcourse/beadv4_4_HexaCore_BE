1318495c2e5f9c88b5efd0e6f5f0cb0596fe396e
jj <syeonie0526@gmail.com>
2026-02-26 14:27:45 +0900

    fix: CommonResponse 역직렬화 실패로 인한 productDetail null 반환 수정 - (#377)

--- notification/src/main/java/com/back/notification/adapter/out/feign/product/ProductFeignClient.java
+++ notification/src/main/java/com/back/notification/adapter/out/feign/product/ProductFeignClient.java
@@ -1,6 +1,6 @@
 package com.back.notification.adapter.out.feign.product;
 
-import com.back.common.response.CommonResponse;
+import com.back.common.event.FeignResponse;
 import com.back.notification.adapter.out.feign.product.dto.ProductDetailListResponse;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
@@ -12,5 +12,5 @@ import java.util.List;
 public interface ProductFeignClient {
 
     @GetExchange("/api/v1/products/variants")
-    CommonResponse<ProductDetailListResponse> getProducts(@RequestParam("productIds") List<Long> productIds);
+    FeignResponse<ProductDetailListResponse> getProducts(@RequestParam("productIds") List<Long> productIds);
 }
